271a6cec3c72122d6d3bc7240572e09f2224bf03
evurupoornachand <epoornachand@gmail.com>
2024-06-05 10:13:57 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,11 @@
+package com.operators;
+
+public class Ternary {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int min = (a < b) ? a : b;
+		System.out.println(min);
+	}
+
+}
