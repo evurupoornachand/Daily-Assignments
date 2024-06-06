0f9419c0ac92387833be565a89a3cf052ba68004
Poorna Chand Evuru <poornachandevuru@Poornas-MacBook-Pro.local>
2024-06-06 10:01:07 +0530

    adding java files

---
+++ src/com/operators/Boolean.java
@@ -0,0 +1,13 @@
+package com.operators;
+
+public class Boolean {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		int p = 5;
+		System.out.println((p < a) & (a > b));
+		System.out.println((p < a) | (a > b));
+		System.out.println((p < a) ^ (a > b));
+	}
+
+}
